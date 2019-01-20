ab2b0306f75178003b414bea4bfe075feb756c4a
Bohdan Menshykov <menshikov93@gmail.com>
2019-01-20 19:36:50 +0200

    Updates

---
+++ precache-manifest.9df2f8daa4765b7e43323c42a8a99ded.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a07cb7f923908e342b845e252a52bb7",
+    "url": "/my-react-app/static/media/polygon.5a07cb7f.jpg"
+  },
+  {
+    "revision": "171d95dd39969154bef5",
+    "url": "/my-react-app/static/js/runtime~main.171d95dd.js"
+  },
+  {
+    "revision": "e4c3f30db081b59d46ac",
+    "url": "/my-react-app/static/js/main.e4c3f30d.chunk.js"
+  },
+  {
+    "revision": "0298ff89554db1325d2a",
+    "url": "/my-react-app/static/js/1.0298ff89.chunk.js"
+  },
+  {
+    "revision": "e4c3f30db081b59d46ac",
+    "url": "/my-react-app/static/css/main.1ae97655.chunk.css"
+  },
+  {
+    "revision": "0298ff89554db1325d2a",
+    "url": "/my-react-app/static/css/1.cd5805b0.chunk.css"
+  },
+  {
+    "revision": "438f51c8e87b4fc74d72dcd57a24cb96",
+    "url": "/my-react-app/index.html"
+  }
+];
